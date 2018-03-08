ba0bc104939d179fddad536521fb01317f9fca85
Ben <benj.teboul@gmail.com>
2018-03-08 10:09:50 +0100

    Add pagination NOT VALIDATE

--- ajax/Pagination.ajax.php
+++ ajax/Pagination.ajax.php
@@ -29,12 +29,12 @@ foreach ($listeMonument as $monument ){
     $listeMonuments[] = array_merge($monument , $comments );
 
 }
-
-foreach ($listeMonuments as $item) {
-    echo '<div class="col-md-4 col-sm-6 col-xs-12" onclick="document.location.href=\'' . $path . 'php/monumentDetail.php?q='.$item['Id_monument'].'\'">
+if($listeMonument && count($listeMonuments) > 0) {
+    foreach ($listeMonuments as $item) {
+        echo '<div class="col-md-4 col-sm-6 col-xs-12" onclick="document.location.href=\'' . $path . 'php/monumentDetail.php?q=' . $item['Id_monument'] . '\'">
 				<div class="thingsBox thinsSpace">
 					<div class="thingsImage">
-						<img src="'.$path.'img/Monuments/'.$item['Id_monument'].'/'.str_replace(" " , "_" , $item['Name_Monument']).'_1.jpg" alt="Image things">
+						<img src="' . $path . 'img/Monuments/' . $item['Id_monument'] . '/' . str_replace(" ", "_", $item['Name_Monument']) . '_1.jpg" alt="Image things">
 						<div class="thingsMask">
 							<ul class="list-inline rating">
                                 {% set Rating = Monument|last.totalRating / Monument|last.totalLine %}
@@ -73,18 +73,21 @@ foreach ($listeMonuments as $item) {
 
 
 							</ul>
-							<a href="'.$path.'php/monumentDetail.php?q='.$item['Id_monument'].'"><h2>'.$item['Name_Monument'].'<i class="fa fa-check-circle" aria-hidden="true"></i></h2></a>
+							<a href="' . $path . 'php/monumentDetail.php?q=' . $item['Id_monument'] . '"><h2>' . $item['Name_Monument'] . '<i class="fa fa-check-circle" aria-hidden="true"></i></h2></a>
 
 						</div>
 					</div>
 					<div class="thingsCaption ">
 						<ul class="list-inline captionItem">
-							<li><i class="fa fa-heart-o" aria-hidden="true"></i>'.$item[sizeof($item)-1]['totalLine'].'</li>
-							<li><a href="#">'.$item['Type_Name'].'</a></li>
+							<li><i class="fa fa-heart-o" aria-hidden="true"></i>' . $item[sizeof($item) - 1]['totalLine'] . '</li>
+							<li><a href="#">' . $item['Type_Name'] . '</a></li>
 						</ul>
 					</div>
 				</div>
 			</div>';
+    }
+}else{
+    echo '';
 }
 echo '<div class="col-xs-12 toFade">
 				<div class="btnArea btnAreaBorder text-center">
