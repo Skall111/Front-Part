47a2f83de4fc5554e6cdb87c4dd20aac4b1649db
Ben <benj.teboul@gmail.com>
2018-03-08 10:11:17 +0100

    Add condition is isset $_GET in jSonDate generated

--- php/JsonData.php
+++ php/JsonData.php
@@ -5,20 +5,40 @@
  * Date: 06/01/2018
  * Time: 21:28
  */
+
 include '../config/variables.inc.php';
 require 'autoload.php';
 $db = App::getDatabase();
 
-$listeMonument = $db->query("SELECT * , Monument.Id AS ID_MON FROM Monument LEFT JOIN Image ON Monument.Id = Image.Id_Monument")->fetchAll();
+$condition = " " ;
+if(isset($_GET)){
+    $condition .= " WHERE 1 ";
+    if(isset($_GET['name'])){
+        $condition .= " AND M.Name LIKE '%".$_GET['name']."%'";
+    }
+    if(isset($_GET['city']) && $_GET['city'] != 0 && trim($_GET['city']) != ""){
+        $condition .= " AND M.Id_City = ".$_GET['city'];;
+    }
+    if(isset($_GET['type']) && $_GET['type'] != 0 && trim($_GET['type']) != ""){
+        $condition .= " AND A.Id_Type = ".$_GET['type'];;
+    }
+}
+
+$listeMonument = $db->query("SELECT * , M.Id AS ID_MON  , M.Name AS NAME_MON , I.Url AS URL_IMG 
+                                    FROM Monument AS M
+                                    LEFT JOIN Avoir AS A ON M.Id = A.Id_Monument 
+                                    LEFT JOIN Type AS T ON A.Id_Type = T.Id
+                                    LEFT JOIN Image AS I  ON M.Id = I.Id_Monument
+                                    " . $condition)->fetchAll();
 $Json_Prepare = [];
 foreach ($listeMonument as $item) {
     $Json_Prepare[] = [
         'id' => $item['ID_MON'],
         'is_logged_in' => $item['ID_MON'],
-        'title' => $item['Name'],
+        'title' => $item['NAME_MON'],
         'address' => '',
         'price' => false,
-        'thumbnail' => $path."img/Monuments/".$item['ID_MON']."/".$item['Url'],
+        'thumbnail' => $path."img/Monuments/".$item['ID_MON']."/".$item['URL_IMG'],
         'verified' => true,
         'latitude' => $item['Lat'],
         'longitude' =>	$item['Lon']
